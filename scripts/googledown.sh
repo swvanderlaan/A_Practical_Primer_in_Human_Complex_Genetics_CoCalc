6f48b851b6e33e934804c3f9e709203b015cc704
Sander W. van der Laan <s.w.vanderlaan@gmail.com>
2022-03-31 12:33:22 +0200

    updates

---
+++ scripts/googledown.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# reference: https://gist.github.com/iamtekeste/3cdfd0366ebfd2c0d805
+if [ $# != 2 ]; then
+echo "Usage: googledown.sh ID save_name"
+exit 0
+fi
+confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$1 -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+echo $confirm
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$confirm&id=$1" -O $2 && rm -rf /tmp/cookies.txt
